e50b8f4292ef8489b8e76ea8401e8b48a32d2199
Talles <perozzot@gmail.com>
2019-02-26 14:44:04 -0300

    Create SplashActivity

--- app/src/main/java/com/tallesperozzo/agileprocesschallenge/utils/Constants.java
+++ app/src/main/java/com/tallesperozzo/agileprocesschallenge/utils/Constants.java
@@ -3,4 +3,6 @@ package com.tallesperozzo.agileprocesschallenge.utils;
 public class Constants {
     public static String RV_ITENS_SAVED = "rv_itens_saved";
     public static int PER_PAGE = 10;
+    public static String BEER_TAG = "BEER";
+    public static int SPLASH_TIME_OUT = 2000;
 }
